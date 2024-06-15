bbe72b2610cb434dbc9bd9a02073cdc903f0d74a
maliByatzes <malib2027@gmail.com>
2024-06-15 04:44:03 +0200

    connect mongodb

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,23 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import pino from 'pino-http';
+import cookieParser from 'cookie-parser';
+import logger from './utils/logger.js';
+import connectMongoDB from './db/connectMongoDB.js';
 
 dotenv.config();
 const app = express();
 
 const PORT = process.env.PORT || 8000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
+app.use(express.json());
+app.use(cookieParser());
+
+if (process.env.NODE_ENV === 'development') {
+  app.use(pino());
+}
 
 app.listen(PORT, () => {
-  console.log(`Server started on port ${PORT}`);
+  logger.info(`Server started on port ${PORT}`);
+  connectMongoDB();
 });
